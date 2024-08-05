f6968de9c4deb9ec7e65d262a35ff16947aae50d
Clemens Dehnert <clemensdehnert@gmail.com>
2024-08-05 13:02:30 +0200

    missing @Profile for test

--- src/main/java/de/clemens/stream/StreamApplication.java
+++ src/main/java/de/clemens/stream/StreamApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
@@ -29,6 +30,7 @@ public class StreamApplication {
     }
 
     @Bean(initMethod = "start", destroyMethod = "stop")
+    @Profile("dev")
     public Server h2Server() throws SQLException {
         return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
